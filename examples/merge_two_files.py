d2323cbb27aac948c39c26532df3ebcc5a9925f4
sth-v <sthv.developer@gmail.com>
2025-01-11 23:57:14 +0300

    - Add merge_two_files.py example - Setup pyproject.toml

--- examples/merge_two_files.py
+++ examples/merge_two_files.py
@@ -1,11 +1,12 @@
 import pygltf
-
-with open('examples/scene-1-1.glb', 'rb') as fl1:
+from pathlib import Path
+examples=Path(__file__).parent
+with open(examples/'scene-1-1.glb', 'rb') as fl1:
     geometry1 = pygltf.load(fl1)
-with open('examples/scene-1-2.glb', 'rb') as fl1:
+with open(examples/'scene-1-2.glb', 'rb') as fl1:
     geometry2 = pygltf.load(fl1)
 
 geometries = geometry1 + geometry2
 
-with open('examples/merge-scene-1.glb', 'wb') as f:
+with open(examples/'merge-scene-1.glb', 'wb') as f:
     pygltf.dump(geometries, f)
